ea43bea751b875a7faca27ba869da5e8eae48dee
indraardianto <indraardianto5@gmail.com>
2025-05-14 14:36:56 +0700

    fix bugs

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const Hapi = require('@hapi/hapi');
 const init = async () => {
     const server = Hapi.server({
         port: 5000,
-        host: 'localhost'
+        host: process.env.NODE_ENV === 'production' ? 'localhost' : '0.0.0.0',
+        routes: {
+            cors: {
+                origin: ['*']
+            }
+        }
     });
 
-    await server.start();
-    console.log('Server running at:', server.info.uri);
+    // await server.start();
+    // console.log('Server running at:', server.info.uri);
 };
 
-init();
+//init();
 
