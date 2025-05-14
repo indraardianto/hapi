28cf95a325fb1d249b95c0ef63f967b2ef48389e
indraardianto <indraardianto5@gmail.com>
2025-05-14 14:55:52 +0700

    fx bug

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
+const init = async () => {
 const server = Hapi.server({
         port: 5000,
         host: process.env.NODE_ENV === 'production' ? 'localhost' : '0.0.0.0',
@@ -9,4 +11,10 @@ const server = Hapi.server({
             },
         },
 });
+server.route(routes);
+await server.start();
+console.log('Server running at:', server.info.uri);
 
+};
+
+init();
